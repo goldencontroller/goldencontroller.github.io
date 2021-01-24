6c6ec9a22fe82e35fb7e6076ffb6e1bea190d95c
Yikuan Sun <yikuansun2015@gmail.com>
2021-01-24 13:46:39 -0500

    fixed bugs

--- games_viewer.js
+++ games_viewer.js
@@ -1,14 +1,13 @@
 urlparams = new URLSearchParams(location.search);
 
 content = new XMLHttpRequest();
-content.open("GET", "https://goldencontroller.github.io/games_info.json");
+content.open("GET", "https://goldencontroller.github.io/games_info.json", false);
 content.send();
-console.log(content.responseText)
 content = JSON.parse(content.responseText);
 
 game_selected = content[urlparams.get("game")];
 
-document.title = game_selected + " | Games Viewer";
+document.title = game_selected.name + " | Games Viewer";
 
 header = document.createElement("h1");
 header.innerText = game_selected.name;
@@ -21,6 +20,7 @@ document.body.appendChild(desc);
 if (game_selected.weburl) {
     browsertitle = document.createElement("h2");
     browsertitle.innerText = "Play now in browser";
+    document.body.appendChild(browsertitle);
     iframe = document.createElement("iframe");
     iframe.src = game_selected.weburl;
     document.body.appendChild(iframe);
@@ -29,8 +29,8 @@ if (game_selected.weburl) {
 downloadstitle = document.createElement("h2");
 downloadstitle.innerText = "Downloads";
 document.body.appendChild(downloadstitle);
-for (key of game_selected.downloads) {
-    document.createElement("li");
+for (key in game_selected.downloads) {
+    li = document.createElement("li");
     li.innerHTML = "<a href='" + game_selected.downloads[key] + "'>" + key + "</a>";
-    document.body.appendChild("li");
+    document.body.appendChild(li);
 }
