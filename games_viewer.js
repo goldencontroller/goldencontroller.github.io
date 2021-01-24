72b6126caf8f80a3c82f45d7098260526fcfedfe
Yikuan Sun <yikuansun2015@gmail.com>
2021-01-24 14:03:20 -0500

    prevented screen scrolling

--- games_viewer.js
+++ games_viewer.js
@@ -23,6 +23,8 @@ if (game_selected.weburl) {
     document.body.appendChild(browsertitle);
     iframe = document.createElement("iframe");
     iframe.src = game_selected.weburl;
+    iframe.style.width = "900px";
+    iframe.style.height = "500px";
     document.body.appendChild(iframe);
     fullscreenbutton = document.createElement("button");
     fullscreenbutton.innerText = "Full screen";
@@ -38,3 +40,9 @@ for (key in game_selected.downloads) {
     li.innerHTML = "<a href='" + game_selected.downloads[key] + "'>" + key + "</a>";
     document.body.appendChild(li);
 }
+
+window.addEventListener("keydown", function(e) {
+    if([" ", "ArrowLeft", "ArrowRight", "ArrowUp", "ArrowDown"].includes(e.key)) {
+        e.preventDefault();
+    }
+}, false);
